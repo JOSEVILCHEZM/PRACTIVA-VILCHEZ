457a8cea8b123a70dcc84109ed29ef634fb7a263
Jose Vilchez <jhtgus971454@gmail.com>
2022-11-02 23:20:42 -0500

    2330JAVAREVIEW

--- Pract12.java
+++ Pract12.java
@@ -2,6 +2,21 @@
 package FULL_JAVA;
 
 
+
 public class Pract12 {
     
+    public static void main(String[] args) {
+        
+        for (int i = 0; i<=10; i++){
+            System.out.println("Ejecución "+ i);
+            
+            if (i == 5){
+                System.out.println("Saltar a la siguiente ejecución");
+                //Saltar a la siguiente 
+                continue;
+    }
+             System.out.println("No me ejecutare en 5");
+    
+}
+    }
 }
