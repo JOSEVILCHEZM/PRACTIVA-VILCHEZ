9c7522790dd52274b884703084ef17a381f2d341
Jose Vilchez <jhtgus971454@gmail.com>
2022-11-02 23:25:50 -0500

    2325JAVAREVIEW

--- Pract4.java
+++ Pract4.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package FULL_JAVA;
 
-/**
- *
- * @author Jose Vilchez
- */
+
 public class Pract4 {
     
+    public static void main(String[] args) {
+        String nombre = "Oregoom";
+        System.out.println("Cantidad de Caracteres: "+ nombre.length());
+        //Salida 
+        // Cantidad de Caracteres: 7
+        
+        for(int i = 0; i < nombre.length(); i++){
+            System.out.println(nombre.charAt(i));
+    }
 }
+
+}
+
