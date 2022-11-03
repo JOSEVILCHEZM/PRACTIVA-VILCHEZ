b5000a2d201fc8bdde894a90293d97f43d617017
Jose Vilchez <jhtgus971454@gmail.com>
2022-11-02 23:13:49 -0500

    2313JAVAREVIEW

--- Pract1.java
+++ Pract1.java
@@ -1,12 +1,20 @@
 
 package FULL_JAVA;
 
+import java.util.Scanner;
 
 public class Pract1 {
 
     
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Ingrese un número entero: ");
+        int n = leer.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n",n,i,(n * i));
         
     }
     
+}
+
 }
