a583756e23a93b1adf707be60361a40f22cccc98
Jose Vilchez <jhtgus971454@gmail.com>
2022-11-02 23:22:03 -0500

    2332JAVAREVIEW

--- Pract2.java
+++ Pract2.java
@@ -4,4 +4,18 @@ package FULL_JAVA;
 
 public class Pract2 {
     
+    public static void main(String[] args) {
+         int[] array1 = {1, 2, 3, 4, 5};
+
+         
+         //Mostrar un dato 
+        System.out.println(array1[0]);
+        //Modificar dato 
+        array1[0] = 100;
+        //Actualizar 
+        System.out.println(array1[0]);
+        //Longitud de array
+        System.out.println(array1.length);
+    }
+    
 }
